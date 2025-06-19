90485635072271d1af459171b078720c5c1bad97
shrutis <shruti.s.2702@gmail.com>
2025-06-19 22:55:16 +0530

    refactoring

--- num_mapper.rb
+++ num_mapper.rb
@@ -1,11 +1,30 @@
 class NumMapper
 
   def self.add str
+    @str = str
     sum = 0
-    return sum if str.empty?
+    return sum if @str.empty?
+    get_all_declared_delimiters
     
-    delim_scanner = DelimScanner.new str
-    delim, str = delim_scanner.lookup_delimiters
+    result = {is_valid: true, message: nil}
+    run_validations result
+    
+    if !result[:is_valid]
+      return result[:message]
+    end
+
+    nums = @str.scan(/\d+/).select{|n| n.to_i < 1000}
+    nums.each do |s|
+      sum += s.to_i
+    end
+
+    sum
+  end
+
+  ## Filters out single char or/and multichar delimiters declared in string prefix
+  def self.get_all_declared_delimiters
+    delim_scanner = DelimScanner.new @str
+    delim = delim_scanner.lookup_delimiters
     @scd = delim.is_a?(String) && delim.to_s.length == 1 ? delim : ''
     @mcd ||= []    
     if delim.is_a?(Array)
@@ -14,31 +33,17 @@ class NumMapper
         @mcd << d if d.length > 1
       end
     end
-    
-    result = {is_valid: true, message: nil}
-    run_validations str, result
-    
-    if !result[:is_valid]
-      return result[:message]
-    end
-
-    nums = str.scan(/\d+/).select{|n| n.to_i < 1000}
-    nums.each do |s|
-      sum += s.to_i
-    end
-
-    sum
   end
 
-  def self.run_validations str, result
-    negitives = str.scan(/-\d+/).map(&:to_i)
+  def self.run_validations result
+    negitives = @str.scan(/-\d+/).map(&:to_i)
     raise "negatives not allowed (found #{negitives.join(', ')})" if !negitives.empty?
 
     ## Delimiters should not be placed adjacent to each other
     append_regex1 = @mcd.empty? ? '' : @mcd.map{|m| "|[#{m[0]}]{#{m.length+1},}"}.join
     invalid_delim_placement_regex = /([#{@scd}\n,]){2,}#{append_regex1}/
 
-    if !str.scan(invalid_delim_placement_regex).empty? 
+    if !@str.scan(invalid_delim_placement_regex).empty? 
       result[:is_valid] = false
       result[:message] = "Invalid Input"
       return
@@ -48,7 +53,7 @@ class NumMapper
     append_regex2 = @mcd.empty? ? '' : @mcd.map{ |m| "(#{m[0]}{#{m.length}})" }.join
     unidentified_delim_regex = /[^,\n#{@scd}0-9#{append_regex2}]/
     
-    if !str.scan(unidentified_delim_regex).empty? 
+    if !@str.scan(unidentified_delim_regex).empty? 
       result[:is_valid] = false
       result[:message] = "Invalid Delimiter"
       return
@@ -65,22 +70,27 @@ class DelimScanner
   end
 
   def single_char_delim_scan
-    scb = @input.scan(/\/\/(.)\n/).flatten[0]
-    sanitized_input = @input.gsub(/\/\/(.)\n/, '')
-    return scb, sanitized_input
+    regex = /\/\/(.)\n/
+    scd = @input.scan(regex).flatten[0]
+    @input.gsub!(regex, '')
+    return scd
   end
 
-  def multi_char_delim_scan
+  def mixed_char_delim_scan
     regex = /\[(.*?)\]/
     mcd = @input.scan(regex).flatten
-    sanitized_input = @input.gsub("//[#{mcd.join('][')}]\n", '')
-    return mcd, sanitized_input
+    @input.gsub!("//[#{mcd.join('][')}]\n", '')
+    return mcd
   end
 
   def lookup_delimiters
-    delim, sanitized_input = single_char_delim_scan
-    delim, sanitized_input = multi_char_delim_scan if delim.nil?
-    return delim, sanitized_input
+    delim = single_char_delim_scan
+    delim = mixed_char_delim_scan if delim.nil?
+    return delim
+  end
+
+  def sanitized_input
+    @input
   end
 
 end
--- test/test_num_mapper.rb
+++ test/test_num_mapper.rb
@@ -62,13 +62,15 @@ class TestNumMapper < Minitest::Test
 
     it "should return single char delimiter with sanitized string" do
       dc = DelimScanner.new "//&\n1,3&6"
-      assert_equal dc.single_char_delim_scan, ['&', '1,3&6']
+      assert_equal dc.single_char_delim_scan, '&'
+      assert_equal dc.sanitized_input, '1,3&6'
     end
 
     it "should return the multichar char delimiter with sanitized string" do
       dc = DelimScanner.new "//[&&]\n1,3&&6"
-      assert_respond_to dc, :multi_char_delim_scan
-      assert_equal dc.multi_char_delim_scan, [['&&'], '1,3&&6']
+      assert_respond_to dc, :mixed_char_delim_scan
+      assert_equal dc.mixed_char_delim_scan, ['&&']
+      assert_equal dc.sanitized_input, '1,3&&6'
     end
 
   end
