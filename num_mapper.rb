7eb47766efd96ae8acf2ba02c83dbaf5a14db1d8
shrutis <shruti.s.2702@gmail.com>
2025-06-19 12:30:03 +0530

    code for -ve num check

--- num_mapper.rb
+++ num_mapper.rb
@@ -3,7 +3,9 @@ class NumMapper
   def self.add str
     sum = 0
     return sum if str.empty?
-
+    
+    run_validations str
+    
     nums = str.scan(/\d+/)
     nums.each do |s|
       sum += s.to_i
@@ -11,4 +13,10 @@ class NumMapper
 
     sum
   end
+
+  def self.run_validations str
+    negitives = str.scan(/-\d+/).map(&:to_i)
+    raise "negatives not allowed (found #{negitives.join(', ')})" if !negitives.empty?
+  end
+
 end
