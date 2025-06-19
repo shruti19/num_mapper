8a9028ce4494bc62aff610a15a76298c6dc73e00
shrutis <shruti.s.2702@gmail.com>
2025-06-19 12:18:34 +0530

    code for adding , or \n separated integers

--- num_mapper.rb
+++ num_mapper.rb
@@ -3,6 +3,12 @@ class NumMapper
   def self.add str
     sum = 0
     return sum if str.empty?
-    
+
+    nums = str.scan(/\d+/)
+    nums.each do |s|
+      sum += s.to_i
+    end
+
+    sum
   end
 end
