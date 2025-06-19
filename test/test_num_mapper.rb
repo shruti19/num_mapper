ce4fa5d2574fd40e6528974990a1d9440bf6abcf
shrutis <shruti.s.2702@gmail.com>
2025-06-19 12:17:09 +0530

    test cases for , & \n delimiters

--- test/test_num_mapper.rb
+++ test/test_num_mapper.rb
@@ -12,7 +12,16 @@ class TestNumMapper < Minitest::Test
       assert_equal NumMapper.add(''), 0 
     end
 
-    
+    it "should return sum of integers present in comma separated string" do
+      assert_equal NumMapper.add('1'), 1
+      assert_equal NumMapper.add('1,2'), 3
+      assert_equal NumMapper.add('0,3,2,4'), 9
+    end
+
+    it "should return sum of integers in a string delimited by comma or newline (\n)" do
+      assert_equal NumMapper.add("1\n2"), 3
+      assert_equal NumMapper.add("0,3\n2,4"), 9
+    end
 
 	end
 end
